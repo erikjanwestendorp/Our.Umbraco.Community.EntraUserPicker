5d8103d4ad3aba488cde3fccc237dbf2864a76c5
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-10-13 19:29:14 +0200

    Use Umbraco's user picker

--- src/Our.Umbraco.Community.EntraUserPicker.Assets/wwwroot/App_Plugins/Client/client.js
+++ src/Our.Umbraco.Community.EntraUserPicker.Assets/wwwroot/App_Plugins/Client/client.js
@@ -1,26 +1,36 @@
-import { property as i, customElement as a, LitElement as m, html as v } from "@umbraco-cms/backoffice/external/lit";
-var c = Object.defineProperty, f = Object.getOwnPropertyDescriptor, l = (o, r, u, t) => {
-  for (var e = t > 1 ? void 0 : t ? f(r, u) : r, p = o.length - 1, s; p >= 0; p--)
-    (s = o[p]) && (e = (t ? s(r, u, e) : s(e)) || e);
-  return t && e && c(r, u, e), e;
-};
-let n = class extends m {
+import { property as v, customElement as l, LitElement as _, html as m } from "@umbraco-cms/backoffice/external/lit";
+import { UmbPropertyValueChangeEvent as d } from "@umbraco-cms/backoffice/property-editor";
+var f = Object.defineProperty, E = Object.getOwnPropertyDescriptor, c = (e) => {
+  throw TypeError(e);
+}, u = (e, t, r, n) => {
+  for (var a = n > 1 ? void 0 : n ? E(t, r) : t, o = e.length - 1, p; o >= 0; o--)
+    (p = e[o]) && (a = (n ? p(t, r, a) : p(a)) || a);
+  return n && a && f(t, r, a), a;
+}, y = (e, t, r) => t.has(e) || c("Cannot " + r), P = (e, t, r) => t.has(e) ? c("Cannot add the same private member more than once") : t instanceof WeakSet ? t.add(e) : t.set(e, r), g = (e, t, r) => (y(e, t, "access private method"), r), i, h;
+let s = class extends _ {
   constructor() {
-    super(...arguments), this.value = "";
+    super(...arguments), P(this, i), this.value = "";
   }
   render() {
-    return v`
-            <umb-user-input min="0" max="1" .value=${this.value ?? ""}></umb-user-input>
-          `;
+    return m`
+			<umb-user-input min="0" max="1" .value=${this.value ?? ""} @change=${g(this, i, h)}></umb-user-input>
+		`;
   }
 };
-l([
-  i({ type: String })
-], n.prototype, "value", 2);
-n = l([
-  a("property-editor-ui-entra-user-picker")
-], n);
+i = /* @__PURE__ */ new WeakSet();
+h = function(e) {
+  this.value = e.target.value, this.dispatchEvent(new d());
+};
+u([
+  v({ type: String })
+], s.prototype, "value", 2);
+u([
+  v({ attribute: !1 })
+], s.prototype, "config", 2);
+s = u([
+  l("property-editor-ui-entra-user-picker")
+], s);
 export {
-  n as default
+  s as default
 };
 //# sourceMappingURL=client.js.map
--- src/Our.Umbraco.Community.EntraUserPicker.Client/Client/src/property-editor-ui-entra-user-picker.element.ts
+++ src/Our.Umbraco.Community.EntraUserPicker.Client/Client/src/property-editor-ui-entra-user-picker.element.ts
@@ -1,16 +1,26 @@
 import { LitElement, html, customElement, property } from "@umbraco-cms/backoffice/external/lit";
 import { UmbPropertyEditorUiElement } from "@umbraco-cms/backoffice/extension-registry";
+import { UmbPropertyEditorConfigCollection, UmbPropertyValueChangeEvent } from "@umbraco-cms/backoffice/property-editor";
 
 @customElement('property-editor-ui-entra-user-picker')
 export default class PropertyEditorUIEntraUserPickerElement extends LitElement implements UmbPropertyEditorUiElement {
     @property({ type: String })
-    public value = "";
+    public value = '';
 
-    render() {
-        return html`
-            <umb-user-input min="0" max="1" .value=${this.value ?? ''}></umb-user-input>
-          `;
-    }
+
+	@property({ attribute: false })
+	public config?: UmbPropertyEditorConfigCollection;
+
+	#onChange(event: CustomEvent & { target: HTMLInputElement  }) {
+		this.value = event.target.value;
+		this.dispatchEvent(new UmbPropertyValueChangeEvent());
+	}
+
+    override render() {
+		return html`
+			<umb-user-input min="0" max="1" .value=${this.value ?? ''} @change=${this.#onChange}></umb-user-input>
+		`;
+	}
 }
 
 declare global {
