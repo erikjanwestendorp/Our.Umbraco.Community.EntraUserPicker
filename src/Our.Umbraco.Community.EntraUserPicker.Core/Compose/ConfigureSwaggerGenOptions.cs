c25a4be722ff88b547eb60a321ef4647c604a7ec
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-11-08 17:47:45 +0100

    Start swagger

---
+++ src/Our.Umbraco.Community.EntraUserPicker.Core/Compose/ConfigureSwaggerGenOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Our.Umbraco.Community.EntraUserPicker.Core.Compose;
+
+internal class ConfigureSwaggerGenOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+
+        options.SwaggerDoc("entra-picker-api-v1", new OpenApiInfo
+        {
+            Title = "Entra Picker API v1", Version = "1.0"
+        });
+    }
+}
