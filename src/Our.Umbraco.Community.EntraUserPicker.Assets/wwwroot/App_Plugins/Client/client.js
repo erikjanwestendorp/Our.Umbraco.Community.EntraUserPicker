14363be1305194332569479e9799ed52c6ce8612
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2025-02-12 17:00:14 +0100

    Bundle

--- src/Our.Umbraco.Community.EntraUserPicker.Assets/wwwroot/App_Plugins/Client/client.js
+++ src/Our.Umbraco.Community.EntraUserPicker.Assets/wwwroot/App_Plugins/Client/client.js
@@ -4,47 +4,48 @@ const e = [
     name: "Entra User Picker",
     alias: "Entra.PropertyEditorUi.UserPicker",
     element: () => import("./property-editor-ui-entra-user-picker.element-BUMFjQW-.js"),
+    elementName: "property-editor-ui-entra-user-picker",
     meta: {
       label: "Entra User Picker",
       propertyEditorSchemaAlias: "Umbraco.Plain.String",
       icon: "icon-user",
-      group: "people",
-      settings: {
-        properties: [
-          {
-            alias: "minNumber",
-            label: "Minimum number of users",
-            propertyEditorUiAlias: "Umb.PropertyEditorUi.Integer",
-            config: [
-              {
-                alias: "min",
-                value: 0
-              }
-            ]
-          },
-          {
-            alias: "maxNumber",
-            label: "Maximum number of users",
-            propertyEditorUiAlias: "Umb.PropertyEditorUi.Integer",
-            config: [
-              {
-                alias: "min",
-                value: 0
-              }
-            ]
-          }
-        ],
-        defaultData: [
-          {
-            alias: "minNumber",
-            value: 0
-          },
-          {
-            alias: "maxNumber",
-            value: 1
-          }
-        ]
-      }
+      group: "people"
+      // settings:{
+      //     properties: [
+      //         {
+      //             alias: "minNumber",
+      //             label: "Minimum number of users",
+      //             propertyEditorUiAlias: "Umb.PropertyEditorUi.Integer",
+      //             config: [
+      //                 {
+      //                     alias: "min",
+      //                     value: 0
+      //                 }                            
+      //             ]
+      //         },
+      //         {
+      //             alias: "maxNumber",
+      //             label: "Maximum number of users",
+      //             propertyEditorUiAlias: "Umb.PropertyEditorUi.Integer",
+      //             config: [
+      //                 {
+      //                     alias: "min",
+      //                     value: 0
+      //                 }
+      //             ]
+      //         }
+      //     ],
+      //     defaultData: [
+      //         {
+      //             alias : "minNumber",
+      //             value : 0
+      //         },
+      //         {
+      //             alias : "maxNumber",
+      //             value : 1
+      //         }
+      //     ]
+      // }
     }
   }
 ];
